f32c830c0927a5854e770efdca09e8b44faac381
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2023-08-17 22:00:12 -0700

    Remove unused dependency

--- code/upstream-integration-tests/src/androidTest/java/com/adobe/marketing/mobile/integration/ExampleInstrumentedTest.kt
+++ code/upstream-integration-tests/src/androidTest/java/com/adobe/marketing/mobile/integration/ExampleInstrumentedTest.kt
@@ -13,7 +13,6 @@ package com.adobe.marketing.mobile.integration
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.json.JSONObject
 
 import org.junit.Test
 import org.junit.runner.RunWith
