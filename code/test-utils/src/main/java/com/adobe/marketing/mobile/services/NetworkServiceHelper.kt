2ac90506e641914edae6393b7f386464cf19b769
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2023-09-07 17:08:16 -0700

    Update method params to optional to align better with Java interface types

--- code/test-utils/src/main/java/com/adobe/marketing/mobile/services/NetworkServiceHelper.kt
+++ code/test-utils/src/main/java/com/adobe/marketing/mobile/services/NetworkServiceHelper.kt
@@ -3,7 +3,7 @@ package com.adobe.marketing.mobile.services
 open class NetworkServiceHelper: Networking {
     private val delegate: NetworkService = NetworkService()
 
-    override fun connectAsync(request: NetworkRequest, callback: NetworkCallback) {
+    override fun connectAsync(request: NetworkRequest?, callback: NetworkCallback?) {
         delegate.connectAsync(request, callback)
     }
 }
