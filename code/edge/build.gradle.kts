1dc8265fc552f3a0fc7e8e2210929cd3953168c7
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2024-05-21 16:13:40 -0700

    Revert test utils dependency change for functional tests

--- code/edge/build.gradle.kts
+++ code/edge/build.gradle.kts
@@ -36,8 +36,8 @@ dependencies {
 
     testImplementation("com.github.adobe:aepsdk-testutils-android:ba5aa1e477")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.12.7")
-
-    androidTestImplementation("com.github.adobe:aepsdk-testutils-android:ba5aa1e477")
+    
+    androidTestImplementation(project(":test-utils"))
     androidTestImplementation("com.adobe.marketing.mobile:edgeconsent:3.0.0-SNAPSHOT")
     {
         exclude(group = "com.adobe.marketing.mobile", module = "edge")
