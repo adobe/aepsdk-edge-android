7d51724ac4fe320fbb04b0e4d89634c77f3a9ad7
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2024-05-21 17:47:03 -0700

    Update AEPTestUtils to latest version

--- code/edge/build.gradle.kts
+++ code/edge/build.gradle.kts
@@ -34,9 +34,9 @@ dependencies {
     implementation("com.adobe.marketing.mobile:core:$mavenCoreVersion")
     implementation("com.adobe.marketing.mobile:edgeidentity:$mavenEdgeIdentityVersion")
 
-    testImplementation("com.github.adobe:aepsdk-testutils-android:ba5aa1e477")
+    testImplementation("com.github.adobe:aepsdk-testutils-android:f8dabeabfa")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.12.7")
-    
+
     androidTestImplementation(project(":test-utils"))
     androidTestImplementation("com.adobe.marketing.mobile:edgeconsent:3.0.0-SNAPSHOT")
     {
