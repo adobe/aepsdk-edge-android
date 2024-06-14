c57619a305a16c89573550385225dce77bc5446e
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2024-06-13 17:43:56 -0700

    Update AEPTestUtils to latest version

--- code/upstream-integration-tests/build.gradle.kts
+++ code/upstream-integration-tests/build.gradle.kts
@@ -88,7 +88,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
 
-    androidTestImplementation("com.github.adobe:aepsdk-testutils-android:0bfafc7296")
+    androidTestImplementation("com.github.adobe:aepsdk-testutils-android:a4991045da")
     androidTestImplementation("com.adobe.marketing.mobile:core:$mavenCoreVersion")
     androidTestImplementation("com.adobe.marketing.mobile:edgeidentity:$mavenEdgeIdentityVersion")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
